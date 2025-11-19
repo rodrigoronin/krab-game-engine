b879fec06ed1a4c826ec3319c1bc5c49913f47ee
rodrigoronin <liramailbox@gmail.com>
2025-11-19 15:53:14 -0300

    chore: adjusting eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,17 +5,15 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   { ignores: ["dist"] },
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-    ],
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "module",
     },
     rules: {
-      '@typescript-eslint/no-unused-vars': 'off'
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
+  prettier,
 );
